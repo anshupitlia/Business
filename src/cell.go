aa008119af5d0b596635387da3d16cdc6355d2e0
Anshu Pitlia <Anshu_Pitlia@external.mckinsey.com>
2017-06-20 00:23:06 +0530

    add other types of cells

--- src/cell.go
+++ src/cell.go
@@ -1,5 +1,10 @@
 package src
 
+const (
+	JailPenalty = 200
+	HotelPrice = 150
+
+)
 type Cell interface {
    changeWorth(int) int
 }
@@ -7,10 +12,32 @@ type Cell interface {
 type EmptyCell struct {
 }
 
+type HotelCell struct {
+}
+
+type JailCell struct {
+}
+
 func NewEmptyCell() EmptyCell{
 	return EmptyCell{}
 }
 
+func NewHotelCell() HotelCell {
+	return HotelCell{}
+}
+
+func NewJailCell() JailCell {
+	return JailCell{}
+}
+
 func(emptyCell EmptyCell) ChangeWorth(currentWorth int) int{
 	return currentWorth
+}
+
+func(hotelCell HotelCell) ChangeWorth(currentWorth int) int{
+	return currentWorth - HotelPrice
+}
+
+func(jailCell JailCell) ChangeWorth(currentWorth int) int{
+	return currentWorth - JailPenalty
 }
--- test/cell_test.go
+++ test/cell_test.go
@@ -13,3 +13,22 @@ func TestEmptyCellDoesNotChangeWorth(t *testing.T) {
 		t.Fatalf("Expected %d but got %d", currentWorth, changedWorth)
 	}
 }
+func TestHotelCellDeductsHundredFiftyFromWorth(t *testing.T) {
+	var cell = src.NewHotelCell()
+	const currentWorth = 500
+	const expectedWorth = 350
+	changedWorth := cell.ChangeWorth(currentWorth)
+	if changedWorth != expectedWorth {
+		t.Fatalf("Expected %d but got %d", expectedWorth, changedWorth)
+	}
+}
+
+func TestJailCellDeductsTwoHundredFromWorth(t *testing.T) {
+	var cell = src.NewJailCell()
+	const currentWorth = 500
+	const expectedWorth = 300
+	changedWorth := cell.ChangeWorth(currentWorth)
+	if changedWorth != expectedWorth {
+		t.Fatalf("Expected %d but got %d", expectedWorth, changedWorth)
+	}
+}
