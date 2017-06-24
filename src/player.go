2bc6f18d83f169277f6ed6c6ce8a74c21c5ac221
Anshu Pitlia <Anshu_Pitlia@external.mckinsey.com>
2017-06-25 00:16:20 +0530

    A Player has a position

--- src/player.go
+++ src/player.go
@@ -5,13 +5,21 @@ import "math/rand"
 const MaxNumberOnDice = 6
 
 type Player struct {
-
+	position int
 }
 
-func NewPlayer() Player {
-	return Player{}
+func NewPlayer(position int) Player {
+	return Player{position: position}
 }
 
 func (player Player) Throw() int{
 	return rand.Intn(MaxNumberOnDice)
+}
+
+func(player Player) Position() int {
+	return player.position
+}
+
+func(player *Player) MoveTo(position int) {
+	player.position = position
 }
--- test/player_test.go
+++ test/player_test.go
@@ -6,9 +6,28 @@ import (
 )
 
 func TestAPlayerShouldThrowDiceAndGetARandomIncrementBetweenOneAndSix(t *testing.T) {
-	var player = src.NewPlayer()
+	var player = src.NewPlayer(0)
 	increment := player.Throw()
 	if increment < 1 || increment > src.MaxNumberOnDice {
 		t.Fatalf("Expected increment to be between 1 and %d but got %d", src.MaxNumberOnDice, increment)
+	}
+}
+
+func TestAPlayerHasACurrentPositionAndReturnsIt(t *testing.T) {
+	var player = src.NewPlayer(0)
+	position := player.Position()
+
+	if position != 0 {
+		t.Fatalf("expected 0 but got %d", position)
+	}
+}
+
+func TestAPlayerCanBeMovedToAnotherPosition(t *testing.T) {
+	var player = src.NewPlayer(0)
+	player.MoveTo(5)
+	newPosition := player.Position()
+
+	if newPosition != 5 {
+		t.Fatalf("expected 5 but got %d", newPosition)
 	}
 }
